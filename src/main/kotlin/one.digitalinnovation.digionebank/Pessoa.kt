aa3e94c5c4074d50b0c54a860672c24a7f30c429
Saotomefirst <jorge.appleseed@gmail.com>
2021-09-23 01:14:01 -0300

    Definindo setter de Pessoa.cpf como privado
    O método set do atributo cpf de Pessoa foi tornado privado

--- src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kt
+++ src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kt
@@ -1,18 +1,26 @@
 package one.digitalinnovation.digionebank
 
 class Pessoa {
-    var _nome: String = "Jorge"
-    var _cpf: String = "123.456.789-10"
+    var nome: String = "Jorge"
 
-    inner class Endereco {
-        var rua: String = "Rua Teste"
-    }
+    /*
+    Todos os atributos nascem com métodos Get e Set por padrão
+    colocando [ private set ], impedimos que a propriedade seja
+    modificada de fora da classe
+     */
+    var cpf: String = "123.456.789-10"
+    private set
+
+//    inner class Endereco {
+//        var rua: String = "Rua Teste"
+//    }
 
 }
 
 fun main() {
     val jorge = Pessoa()
-    println(jorge._nome)
-    println(jorge._cpf)
-    println(jorge.Endereco().rua)
+    println(jorge.nome)
+    println(jorge.cpf)
+
+//    println(jorge.Endereco().rua)
 }
