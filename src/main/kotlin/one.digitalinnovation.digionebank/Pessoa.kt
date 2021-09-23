9347a553c0cb6ca597384ff477836884900ffb99
Saotomefirst <jorge.appleseed@gmail.com>
2021-09-23 01:05:31 -0300

    Exemplo de Classe Interna
    Criada classe interna Endereco() como exemplo de uso para
    uma classe dentro de outra classe

--- src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kt
+++ src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kt
@@ -3,10 +3,16 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var _nome: String = "Jorge"
     var _cpf: String = "123.456.789-10"
+
+    inner class Endereco {
+        var rua: String = "Rua Teste"
+    }
+
 }
 
 fun main() {
     val jorge = Pessoa()
     println(jorge._nome)
     println(jorge._cpf)
+    println(jorge.Endereco().rua)
 }
