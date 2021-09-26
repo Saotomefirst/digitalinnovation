76a80d80083dc7610a16fd15a60aa6ce8fe1761c
Saotomefirst <jorge.appleseed@gmail.com>
2021-09-26 17:27:49 -0300

    Acertando as Decalarações de Atributos de Pessoa
    Os atributos de Pessoa foram modificados para se tornarem editáves [ var ]
    
    Os atributos de mesmo nome da classe Pessoa na classe Funcionário tiveram seus tipos retirados, indicando que vêm da classe Pessoa

--- src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kt
+++ src/main/kotlin/one.digitalinnovation.digionebank/Pessoa.kt
@@ -2,6 +2,6 @@ package one.digitalinnovation.digionebank
 
 // Exemplo de declaração de classe Abstrata
 open class Pessoa (
-    open val nome : String,
-    open val cpf : String
+    var nome : String,
+    var cpf : String
 )
