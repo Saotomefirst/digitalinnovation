528dfe2f6a580fea92dbed5a0f7a11d994f19871
Saotomefirst <jorge.appleseed@gmail.com>
2021-09-26 15:52:56 -0300

    Enum - parte 2
    Construtor da classe ClienteTipo atualizada com um val, permitindo que se recupere o valor [ descricao ]
    
    Adicionadas outras formas de chamar os dados do Enum na classe de teste

--- src/main/kotlin/one.digitalinnovation.digionebank/testes/TesteClienteTipoEnum.kt
+++ src/main/kotlin/one.digitalinnovation.digionebank/testes/TesteClienteTipoEnum.kt
@@ -6,11 +6,18 @@ fun main () {
 
     //Quando chamamos o [ forEach ], o Kotlin define sozinho o elemento a ser usado como [ it ]
     ClienteTipo.values().forEach {
-        println ("${it.name} -> $it")
+        println ("${it.name} -> ${it.descricao}")
     }
 
     // Alternativamente, podemos dar um nome ao elemento a ser usado
     ClienteTipo.values().forEach { elemento ->
-        println ("${elemento.name} -> $elemento")
+        println ("${elemento.name} -> ${elemento.descricao}")
     }
+
+    // Podemos tambem carregar em uma variável e usar assim
+    val pf = ClienteTipo.PF
+    println ("${pf.name} -> ${pf.descricao}")
+
+    val pj = ClienteTipo.PJ
+    println ("${pj.name} -> ${pj.descricao}")
 }
